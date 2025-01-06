9250a4df288ad98bc35c72968ff99b9f3451cd19
Pacifisto092 <a.duvanov092@gmail.com>
2025-01-06 22:32:07 +0300

    Assert_Multi_select

--- pytestpac/test_multi_select.py
+++ pytestpac/test_multi_select.py
@@ -6,7 +6,10 @@ from playwright.sync_api import Page, expect
 
 def test_example(page: Page) -> None:
     page.goto("https://selenium08.blogspot.com/2019/11/dropdown.html")
-    page.locator("select[name='Month']").select_option(['November', 'July',], label='March')
+    loc = page.locator("select[name='Month']")
+    expect(loc).to_have_value(['November', 'July','March'])
+    loc.select_option(['November', 'July'], label='March')
     time.sleep(2)
-    page.locator("select[name='Month']").select_option(index=2, value='May', label='October')
+    expect(loc).to_have_value([re.compile(r"F"), re.compile(r"M"), re.compile(r"O")])
+    loc.select_option(index=2, value='May', label='October')
     time.sleep(2)
