4cf0a4e665b96cbeda647e13d2d936ef28384676
Pacifisto092 <a.duvanov092@gmail.com>
2025-01-11 00:29:48 +0300

    fix test_multi_select and test_handle_frames

--- pytestpac/test_multi_select.py
+++ pytestpac/test_multi_select.py
@@ -7,9 +7,10 @@ from playwright.sync_api import Page, expect
 def test_example(page: Page) -> None:
     page.goto("https://selenium08.blogspot.com/2019/11/dropdown.html")
     loc = page.locator("select[name='Month']")
-    expect(loc).to_have_value(['November', 'July','March'])
-    loc.select_option(['November', 'July'], label='March')
-    time.sleep(2)
-    expect(loc).to_have_value([re.compile(r"F"), re.compile(r"M"), re.compile(r"O")])
-    loc.select_option(index=2, value='May', label='October')
-    time.sleep(2)
+    loc.select_option(['May', 'July', 'Sept'])
+    expect(loc).to_have_values(['May', 'July', 'Sept'])
+    page.wait_for_timeout(1000)
+    loc.select_option(index=2, value='Ma', label='October')
+    expect(loc).to_have_values([re.compile(r"F"), re.compile(r"M"), re.compile(r"O")])
+    page.wait_for_timeout(1000)
+
