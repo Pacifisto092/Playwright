3700443676fefbdc695a961dace6c0d7dc4e751e
Pacifisto092 <a.duvanov092@gmail.com>
2025-01-15 01:07:07 +0300

    add assert

--- pytestpac/test_get_request_api.py
+++ pytestpac/test_get_request_api.py
@@ -1,17 +1,17 @@
 import requests
 from playwright.sync_api import Playwright, sync_playwright, expect
 
-def run(playwright: Playwright) -> None:
+def test_run(playwright: Playwright) -> None:
 
     #status code 200
     resp = requests.get("https://reqres.in/api/users/2")
-    expect(resp).to_be_ok()
+    assert resp.status_code == 200
     print(resp.status_code)
 
     # status code 404
     resp = requests.get("https://reqres.in/api/users/23")
-    expect(resp).not_to_be_ok()
+    assert resp.status_code == 404
     print(resp.status_code)
 
 with sync_playwright() as playwright:
- run(playwright)
+    test_run(playwright)
